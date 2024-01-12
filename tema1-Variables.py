4c1d38ed126c320040b403500c171d6299d87d55
IDGS-802-20002241 <156141194+IDGS-802-20002241@users.noreply.github.com>
2024-01-11 23:07:37 -0600

    OMG_11/01/2023
    OMG_11/01/2023

---
+++ tema1-Variables.py
@@ -0,0 +1,17 @@
+#Nombre de la variable
+
+numero1= "123"
+numero2='10'
+nombre="German"
+correo="german@gmail.com"
+
+
+print(numero1+numero2)
+print(int(numero1)+int(numero2))
+
+sueldo ="123.203"
+valorDecimal=float(sueldo)
+print(valorDecimal)
+
+edad=110
+print(len(str(edad)))
